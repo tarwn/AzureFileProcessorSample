119de1a1e3fe1c35e1df91faeb4c5d601652e103
Eli Weinstock-Herman <tarwn@tiernok.com>
2012-05-12 20:29:50 -0400

    processor and tests

---
+++ Processor_Core.Tests/ItemProcessorTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Processor_Core.Tests {
+	[TestFixture]
+	public class ItemProcessorTests {
+
+		[Test]
+		public void ProcessItem_LowerCaseData_IsUpperCased() {
+			string data = "my test data";
+			var rawItem = new FullItem() {
+				File = new UTF8Encoding().GetBytes(data),
+				FileName = "test.txt",
+				Received = DateTime.Now.ToUniversalTime(),
+				ResourceId = Guid.NewGuid()
+			};
+			var processor = new ItemProcessor();
+
+			var finishedItem = processor.ProcessItem(rawItem);
+			string result = new UTF8Encoding().GetString(finishedItem.File);
+
+			Assert.AreEqual(data.ToUpper(), result);
+		}
+
+		[Test]
+		public void ProcessItem_RawData_MarksItemAsProcessed() {
+			string data = "my test data";
+			var rawItem = new FullItem() {
+				File = new UTF8Encoding().GetBytes(data),
+				FileName = "test.txt",
+				Received = DateTime.Now.ToUniversalTime(),
+				ResourceId = Guid.NewGuid()
+			};
+			var processor = new ItemProcessor();
+
+			var finishedItem = processor.ProcessItem(rawItem);
+
+			Assert.IsTrue(finishedItem.IsProcessed);
+		}
+
+	}
+}
---
+++ Processor_Core/ItemProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Processor_Core {
+	public class ItemProcessor {
+
+		//purposefully slow processing method w/ less than ideal memory usage to simulate real work
+		public FullItem ProcessItem(FullItem rawItem) {
+			string originalData = new UTF8Encoding().GetString(rawItem.File);
+			string[] dataRows = originalData.Split('\n');
+
+			List<string> result = new List<string>();
+			foreach (string s in dataRows) {
+				string s2 = s;
+				foreach (char c in s.ToCharArray()) {
+					s2 = s2.Replace(c.ToString(), c.ToString().ToUpper());
+				}
+				result.Add(s2);
+			}
+
+			rawItem.IsProcessed = true;
+			rawItem.File = new UTF8Encoding().GetBytes(String.Join("\n", result));
+			return rawItem;
+		}
+
+	}
+}
