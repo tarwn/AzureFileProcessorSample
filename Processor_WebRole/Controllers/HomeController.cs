b75300572f83249974f19c3084cd8b0bf3af74ee
Eli Weinstock-Herman <tarwn@tiernok.com>
2012-05-12 21:51:37 -0400

    web additions for file upload and status output on index

--- Processor_WebRole/Controllers/HomeController.cs
+++ Processor_WebRole/Controllers/HomeController.cs
@@ -3,14 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Processor_Core;
+using Processor_Core.Storage.Azure;
+using Processor_Core.Storage;
+using Processor_WebRole.Models;
 
 namespace Processor_WebRole.Controllers {
 	public class HomeController : Controller {
+
+		IStorageLocator _storageLocator;
+
+		public HomeController() {
+			_storageLocator = new StorageManager();
+		}
+
 		public ActionResult Index() {
 			ViewBag.Message = "Welcome to ASP.NET MVC!";
 
-			return View();
+			var store = new ItemStore(_storageLocator);
+			var model = new StatusViewModel() {
+				ProcessedItems = store.GetProcessedList(),
+				UnprocessedItems = store.GetUnprocessedList()
+			};
+
+			return View(model);
 		}
 
+		[HttpPost]
+		public ActionResult AddFile(HttpPostedFile file) {
+			if (file != null && file.ContentLength > 0) {
+				var item = new FullItem() {
+					ResourceId = Guid.NewGuid(),
+					Received = DateTime.Now.ToUniversalTime(),
+					IsProcessed = false,
+					FileName = file.FileName
+				};
+				item.ReadFileFromStream(file.InputStream);
+
+				new ItemStore(_storageLocator).AddNewItem(item);
+			}
+
+			return View();
+		}
 	}
 }
---
+++ Processor_WebRole/Models/StatusViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Processor_Core;
+
+namespace Processor_WebRole.Models {
+	public class StatusViewModel {
+		public IEnumerable<ItemBase> UnprocessedItems { get; set; }
+		public IEnumerable<ItemBase> ProcessedItems { get; set; }
+	}
+}
